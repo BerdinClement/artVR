b67c44e713b5b6f28f044e59c91d79e1b2a9907e
AntoineB <antoinebacquet62@gmail.com>
2024-02-18 23:26:06 +0100

    test

---
+++ Assets/Scipts/TURNERScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TURNERScript : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject canvas;
+
+    public void Start()
+    {
+        panel.transform.position = canvas.transform.position;
+        panel.transform.localScale = new Vector3(1, 1, 1);
+        panel.SetActive(false);
+    }
+
+    public void OpenPanel()
+    {
+        if (panel == null) return;
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        if (panel == null) return;
+        panel.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OpenPanel();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ClosePanel();
+        }
+    }
+}
